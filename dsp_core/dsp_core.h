55da452eee52259285d0929528d34fa4fbfabf39
johnjanic <janiczek.john@gmail.com>
2026-03-15 21:29:53 -0600

    Add HarmonicScaling for per-harmonic nonlinear slider mapping

--- dsp_core/dsp_core.h
+++ dsp_core/dsp_core.h
@@ -31,6 +31,7 @@
 #include "Source/AudioHistoryBuffer.h"
 #include "Source/StereoHistoryBuffer.h"
 #include "Source/HarmonicLayer.h"
+#include "Source/HarmonicScaling.h"
 #include "Source/LayeredTransferFunction.h"
 #include "Source/SeamlessTransferFunction.h"
 #include "Source/SeamlessTransferFunctionImpl.h"
